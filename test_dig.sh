f8775d70c85a7b645e827af4276b7d90a978ad87
hammad <haashah@gmail.com>
2017-07-11 11:00:32 +0000

    Fixed bug in test_dig.sh
    The format for dig command was missing a formatter
    
    Signed-off-by: hammad <haashah@gmail.com>

--- test_dig.sh
+++ test_dig.sh
@@ -11,7 +11,8 @@ iter=$2
 urls=('yahoo.com' 'bing.com' 'google.com' 'facebook.com' 'instagram.com')
 for x in $(seq 1 $iter); do
   rand_idx=$[RANDOM % 5]
-  output=$(dig $ip ${urls[$rand_idx]})
+  output=$(dig "@$ip" ${urls[$rand_idx]})
   echo "$output"
+  #dig "@$ip" ${urls[$rand_idx]}
   sleep 1
-done 
+done
